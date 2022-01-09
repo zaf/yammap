4875c18c539aa4b61ca1df5d1d5eca1e699077a1
Lefteris Zafiris <zaf@fastmail.com>
2022-01-09 23:21:29 +0200

    Added and improved tests

--- yammap_test.go
+++ yammap_test.go
@@ -12,6 +12,8 @@ Package yammap provides an interface to memory mapped files.
 package yammap
 
 import (
+	"bytes"
+	"math/rand"
 	"os"
 	"strconv"
 	"testing"
@@ -27,41 +29,48 @@ func tmpname() string {
 	return dir + "/" + prefix + strconv.Itoa(int(1e9 + rand%1e9))[1:]
 }
 
-func TestReadWrite(t *testing.T) {
+const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+// random data generator
+func rndmessage(n int) []byte {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = chars[rand.Intn(len(chars))]
+	}
+	return b
+}
+
+func TestOpenFile(t *testing.T) {
 	name := tmpname()
 	m, err := OpenFile(name, O_RDWR|O_CREATE, 0644)
 	if err != nil {
 		t.Error(err)
 	}
+	m.Close()
 	defer os.Remove(name)
-	msg := "Hello World!"
-	_, err = m.Write([]byte(msg))
+	f, err := os.Stat(name)
 	if err != nil {
 		t.Error(err)
 	}
-	err = m.Sync()
+	if f.Size() != int64(pageSize) {
+		t.Error("wrong size of created file")
+	}
+}
+
+func TestCreate(t *testing.T) {
+	name := tmpname()
+	m, err := Create(name, int64(pageSize), O_RDWR|O_CREATE, 0644)
 	if err != nil {
 		t.Error(err)
 	}
-	err = m.Close()
+	m.Close()
+	defer os.Remove(name)
+	f, err := os.Stat(name)
 	if err != nil {
 		t.Error(err)
 	}
-	m2, err := OpenFile(name, O_RDONLY, 0644)
-	if err != nil {
-		t.Error(err)
-	}
-	b := make([]byte, len(msg))
-	_, err = m2.Read(b)
-	if err != nil {
-		t.Error(err)
-	}
-	err = m2.Close()
-	if err != nil {
-		t.Error(err)
-	}
-	if string(b) != msg {
-		t.Error("Wrong data")
+	if f.Size() != int64(pageSize) {
+		t.Error("wrong size of created file")
 	}
 }
 
@@ -79,45 +88,11 @@ func TestTurncate(t *testing.T) {
 		t.Error(err)
 	}
 	if newsize != m.Size() {
-		t.Error("Wrong size")
+		t.Error("wrong size")
 	}
 	err = m.Close()
 	if err != nil {
 		t.Error(err)
-	}
-}
-
-func TestOpenFile(t *testing.T) {
-	name := tmpname()
-	m, err := OpenFile(name, O_RDWR|O_CREATE, 0644)
-	if err != nil {
-		t.Error(err)
-	}
-	m.Close()
-	defer os.Remove(name)
-	f, err := os.Stat(name)
-	if err != nil {
-		t.Error(err)
-	}
-	if f.Size() != int64(pageSize) {
-		t.Error("Wrong size")
-	}
-}
-
-func TestCreate(t *testing.T) {
-	name := tmpname()
-	m, err := Create(name, int64(pageSize), O_RDWR|O_CREATE, 0644)
-	if err != nil {
-		t.Error(err)
-	}
-	m.Close()
-	defer os.Remove(name)
-	f, err := os.Stat(name)
-	if err != nil {
-		t.Error(err)
-	}
-	if f.Size() != int64(pageSize) {
-		t.Error("Wrong size")
 	}
 }
 
@@ -130,7 +105,7 @@ func TestName(t *testing.T) {
 	defer m.Close()
 	defer os.Remove(name)
 	if name != m.Name() {
-		t.Error("Wrong name")
+		t.Error("wrong name of created file")
 	}
 }
 
@@ -148,7 +123,7 @@ func TestSeek(t *testing.T) {
 		t.Error(err)
 	}
 	if position != m.Offset() {
-		t.Error("Wrong offset")
+		t.Error("wrong offset")
 	}
 	current := m.Offset()
 	_, err = m.Seek(position, SEEK_CURRENT)
@@ -156,7 +131,7 @@ func TestSeek(t *testing.T) {
 		t.Error(err)
 	}
 	if current+position != m.Offset() {
-		t.Error("Wrong offset")
+		t.Error("wrong offset")
 	}
 	position = -position
 	_, err = m.Seek(position, SEEK_END)
@@ -164,15 +139,110 @@ func TestSeek(t *testing.T) {
 		t.Error(err)
 	}
 	if m.Size()+position != m.Offset() {
-		t.Error("Wrong offset")
+		t.Error("wrong offset")
 	}
 	_, err = m.Seek(1024, SEEK_END)
 	if err.Error() != "offset goes beyond the end of file" {
-		t.Error("Allowed to seek beyond the end of file")
+		t.Error("allowed to seek beyond the end of file")
 	}
 	_, err = m.Seek(-1024, SEEK_START)
 	if err.Error() != "negative position" {
-		t.Error("Allowed to seek with negative position")
+		t.Error("allowed to seek with negative position")
+	}
+}
+
+func TestReadWrite(t *testing.T) {
+	name := tmpname()
+	m, err := OpenFile(name, O_RDWR|O_CREATE, 0644)
+	if err != nil {
+		t.Error(err)
+	}
+	defer os.Remove(name)
+	msg := rndmessage(pageSize * 2)
+	n, err := m.Write(msg)
+	if err != nil {
+		t.Error(err)
+	}
+	if n != len(msg) {
+		t.Error("wrong number of bytes written")
+	}
+	if m.offset != int64(len(msg)) {
+		t.Error("wrong offset after write")
+	}
+	err = m.Sync()
+	if err != nil {
+		t.Error(err)
+	}
+	err = m.Close()
+	if err != nil {
+		t.Error(err)
+	}
+	m2, err := OpenFile(name, O_RDONLY, 0644)
+	if err != nil {
+		t.Error(err)
+	}
+	b := make([]byte, len(msg))
+	n, err = m2.Read(b)
+	if err != nil {
+		t.Error(err)
+	}
+	if n != len(b) {
+		t.Error("wrong number of bytes read")
+	}
+	if m2.offset != int64(len(msg)) {
+		t.Error("wrong offset after read")
+	}
+	err = m2.Close()
+	if err != nil {
+		t.Error(err)
+	}
+	if bytes.Compare(b, msg) != 0 {
+		t.Error("wrong data read")
+	}
+}
+
+func TestReadAtWriteAt(t *testing.T) {
+	name := tmpname()
+	offset := int64(512)
+	m, err := OpenFile(name, O_RDWR|O_CREATE, 0644)
+	if err != nil {
+		t.Error(err)
+	}
+	defer os.Remove(name)
+	msg := rndmessage(pageSize * 2)
+	n, err := m.WriteAt([]byte(msg), offset)
+	if err != nil {
+		t.Error(err)
+	}
+	if n != len(msg) {
+		t.Error("wrong number of bytes written")
+	}
+	err = m.Sync()
+	if err != nil {
+		t.Error(err)
+	}
+	err = m.Close()
+	if err != nil {
+		t.Error(err)
+	}
+	m2, err := OpenFile(name, O_RDONLY, 0644)
+	if err != nil {
+		t.Error(err)
+	}
+	b := make([]byte, len(msg))
+	n, err = m2.ReadAt(b, offset)
+	if err != nil {
+		t.Error(err)
+	}
+	if n != len(b) {
+		t.Error("wrong number of bytes read")
+	}
+	err = m2.Close()
+	if err != nil {
+		t.Error(err)
+	}
+	if bytes.Compare(b, msg) != 0 {
+		t.Error("wrong data read")
 	}
 }
 
@@ -184,12 +254,11 @@ func BenchmarkWrite(b *testing.B) {
 	}
 	defer m.Close()
 	defer os.Remove(name)
-	data := make([]byte, 512)
+	data := rndmessage(512)
 	b.SetBytes(int64(len(data)))
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		m.Write(data)
-		//m.Sync()
 	}
 }
 
@@ -201,11 +270,10 @@ func BenchmarkOsWrite(b *testing.B) {
 	}
 	defer f.Close()
 	defer os.Remove(name)
-	data := make([]byte, 512)
+	data := rndmessage(512)
 	b.SetBytes(int64(len(data)))
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		f.Write(data)
-		//f.Sync()
 	}
 }
